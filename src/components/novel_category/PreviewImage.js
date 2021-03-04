aa2d4c8d3a2fde40d7edc9c96d45e647bd2e60c3
tayutaedomo <tayutaedomo@gmail.com>
2021-03-05 00:37:59 +0900

    add: reset button

--- src/components/novel_category/PreviewImage.js
+++ src/components/novel_category/PreviewImage.js
@@ -1,13 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component, createRef } from 'react';
 
 export class PreviewImage extends Component {
   constructor(props) {
     super(props);
+
     this.state = { imageData: null };
+
+    this.fileInput = createRef();
   }
 
   onFileChange(e) {
-    console.log('onFileChange', e);
     const files = e.target.files;
 
     if (files.length > 0) {
@@ -22,12 +24,27 @@ export class PreviewImage extends Component {
     }
   }
 
+  resetInput() {
+    this.fileInput.current.value = '';
+    this.setState({ imageData: null });
+  }
+
   render() {
     const imageData = this.state.imageData;
     let preview = '';
+    let resetButton = '';
 
     if (imageData !== null) {
-      preview = <img src={imageData} width="64" />;
+      preview = (
+        <div>
+          <img src={imageData} width="64" />
+        </div>
+      );
+      resetButton = (
+        <button type="button" onClick={() => this.resetInput()}>
+          Reset
+        </button>
+      );
     }
 
     return (
@@ -37,7 +54,9 @@ export class PreviewImage extends Component {
           accept="image/png,image/jpeg,image/jpg"
           placeholder="Image File"
           onChange={(e) => this.onFileChange(e)}
+          ref={this.fileInput}
         />
+        {resetButton}
         {preview}
       </div>
     );
